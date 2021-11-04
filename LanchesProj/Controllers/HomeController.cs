47b716e1a85cd4c3fa0b581ea91439d425e607c7
Fernando Lima <fernando.ilustrar@gmail.com>
2021-11-04 18:46:01 -0300

    aula 19

--- LanchesProj/Controllers/HomeController.cs
+++ LanchesProj/Controllers/HomeController.cs
@@ -27,5 +27,15 @@ namespace LanchesProj.Controllers
         {
             return View();
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+
     }
 }
