043285b1de4d2990741a15244d97b62c3e380b2e
Fernando Lima <fernando.ilustrar@gmail.com>
2021-11-05 10:12:07 -0300

    aula 24

---
+++ LanchesProj/Controllers/LancheController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanchesProj.Repositories;
+
+namespace LanchesProj.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository)
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Lanche = "Lanches";
+            ViewData["Categoria"] = "Categoria";
+
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
